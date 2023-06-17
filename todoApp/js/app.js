833cab7428da4133c3f644d14298a42fa670daa0
Miraghaa <miraghaea@code.edu.az>
2023-06-17 23:14:23 +0400

    updated

--- todoApp/js/app.js
+++ todoApp/js/app.js
@@ -1,30 +1,42 @@
 let btn = document.querySelector('.button')
 let p = document.getElementById('asd')
-p.innerHTML='No task'
+let content= document.querySelector('.content')
+let ins = document.querySelector('.input')
+p.innerHTML='No Task'
 
 btn.onclick = () => {
-    let inp = document.querySelector('.input').value
-    let h2 = document.createElement('h2')
+  if(ins == ''){
+    ins.innerHTML='yazi yazin'
+  }
+    let inp = ins.value 
+    let h2 = document.createElement('h4')
     let btn1 = document.createElement('button')
     let div = document.createElement('div')
+    h2.innerHTML=inp
+    h2.style.color = 'white'
+    content.style.padding = '10px 0px'
     div.append(h2,btn1)
     document.querySelector('.content').appendChild(div)
     div.className='miri'
     btn1.className='btn1'
-    h2.innerHTML=inp
-    btn1.innerHTML='Delete'
+    btn1.innerHTML='Del'
    let btns = document.querySelectorAll('.btn1')
    for (let btn3 of btns){
     btn3.onclick=()=>{
       btn3.parentElement.remove()
       let task = document.querySelectorAll('.miri')
-      p.innerHTML=`Task number ${task.length}`
+      p.innerHTML=`You have ${task.length} tasks`
+        
+       if(task.length === 0){
+        p.innerHTML='No Task'
+        content.style.padding = '0px 0px'
+       }
     }
    }
    let task = document.querySelectorAll('.miri')
    p.innerHTML=`You have ${task.length} tasks`
-
 }
+
 
 
 let src = document.querySelector('.search')
